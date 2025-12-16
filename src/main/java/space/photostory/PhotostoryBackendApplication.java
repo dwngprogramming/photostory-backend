1c5699df94517548a083450248856045c06ec4ec
dwngprogramming <dwnq.coding@gmail.com>
2025-12-16 17:56:31 +0700

    chore: initialize Spring Boot project for Photostory API

---
+++ src/main/java/space/photostory/PhotostoryBackendApplication.java
@@ -0,0 +1,13 @@
+package space.photostory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotostoryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotostoryBackendApplication.class, args);
+    }
+
+}
